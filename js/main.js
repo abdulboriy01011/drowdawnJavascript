4fedbf592d505015af601a6cdcae6bdbe8aebc99
abdulboriy01011 <abdulboriyburhonjonov00@gmail.com>
2024-08-09 19:53:24 +0500

    add css and  javascirpt

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const elMenuBtn = document.querySelectorAll('.menu__link');
+const elTop = document.querySelectorAll('.fax__top');
+console.log(elTop);
+
 elMenuBtn.forEach(item => {
 	item.addEventListener('click', e => {
-		e.target.classList.togle('.menu__link-active');
+		item.classList.toggle('.menu__link-active');
+	});
+});
+
+// Eltop
+elTop.forEach(item => {
+	item.addEventListener('click', e => {
+		console.log(item.nextElementSibling);
+		item.nextElementSibling.classList.toggle('visable');
 	});
 });
